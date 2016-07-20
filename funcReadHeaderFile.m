5604f37af3dfe612a8f0858de748a14e51edf968
Daniel Peirano <djpeirano@gmail.com>
2016-07-20 13:06:08 -0700

    Added the ability to reflect the voltage of dispersion plots.  Additionally added a function to load data from a _Hdr file and also to find specific rows requested by the user from the _Hdr file (funcReadHeaderFile.m)

--- funcReadHeaderFile.m
+++ funcReadHeaderFile.m
@@ -1,4 +1,5 @@
-function cellOutput = funcReadHeaderFile(filename)
+function [cellOutput, cellRequestedOutput]...
+    = funcReadHeaderFile(filename, cellRequested)
 
 % Author: Daniel J. Peirano
 % Originally Written: 19Jul2016
@@ -12,6 +13,15 @@ function cellOutput = funcReadHeaderFile(filename)
 % cellOutput --- First column is Section that the variable was found in,
             % second column is the measurement name, and third column is
             % the value.
+            
+% cellRequested is a cell of varying number of rows and columns can be up
+% to 3 where each row is corresponds 
+% with the requested information. If cellRequested is {'Sequencer'} then
+% all rows with the FIRST COLUMN equal to 'Sequencer' is returned. If
+% a row from cellRequested is {'', 'RF Voltage (V)'}, then all rows with
+% the second column equal to 'RF Voltage (V)' will be returned.  Items will
+% be returned in string form, and there will be NO test for uniqueness of
+% returned rows.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Debug
@@ -20,6 +30,12 @@ function cellOutput = funcReadHeaderFile(filename)
 % clear
 % filename = 'C:\Users\Daniel\Desktop\Work\Yasas\Dispersion Plots\Data\20160607 Collection\2_butanone_500_1500 Vrf_26 C_10VStep_100 Steps_50mlmin_test1_Hdr.xls';
 % filename = 'C:\Users\Daniel\Dropbox\Projects\AnalyzeIMS\Data\Demo\Quality Controls\Run_3_Hdr.xls'
+
+% cellRequested = {'Sequencer'};
+% cellRequested = {'' 'RF Voltage (V)'};
+% cellRequested = {'Sequencer' '' '';...
+%     '' 'RF Voltage (V)' '';...
+%     'Sequencer' '' 'GC Fan On'};
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Load Data
@@ -62,6 +78,7 @@ for i=1:length(cellRawData)
                 % section of the methodology based on the previous line
                 strCurrSection = cellOutput{numRow,2};
             else
+                numRow = numRow + 1;
                 cellOutput{numRow,1} = strCurrSection;
                 cellOutput{numRow,2} = strCurr(vecLocTabs(1)+1:vecLocTabs(2)-1);
                 cellOutput{numRow,3} = strCurr(vecLocTabs(2)+1:end);
@@ -75,4 +92,28 @@ end
 cellOutput = cellOutput(1:numRow,:);
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Identify Requested Rows
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+cellRequestedOutput = cell(0,3);
+if nargin == 2
+    for i=1:size(cellRequested,1)
+        cellRow = cellRequested(i,:);
+        vecBoolRequested = true(size(cellOutput,1), 1);
+        for j=1:length(cellRow)
+            if ~isempty(cellRow{j})
+                vecBoolRequested = vecBoolRequested...
+                    & strcmp(cellOutput(:,j), cellRow{j});
+            end
+        end
+        if any(vecBoolRequested)
+            cellRequestedOutput = [cellRequestedOutput;...
+                cellOutput(vecBoolRequested, :)]; %#ok<AGROW>
+        else
+            warning('funcReadHeaderFile: Requested row not found in %s',...
+                filename)
+        end
+    end
+end
+    
 
